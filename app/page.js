8292523ebc527eff795ad5a59784685d38e89f47
sabah-naveed <sabahnaveed03@gmail.com>
2024-09-08 19:41:47 -0400

    feat: add 3d home page

--- app/page.js
+++ app/page.js
@@ -1,19 +1,23 @@
 "use client";
 
-import Image from "next/image";
 import React from "react";
-
 import Typewriter from "typewriter-effect";
+import SabahAvatar from "@/components/SabahAvatar";
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="ml-12">
+    <div className="flex items-center justify-between h-screen bg-gray-950 mr-10">
+      <div>
+        <SabahAvatar />
+      </div>
+      <div className="flex flex-col ">
         <h1 className="font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2">
-          Hi, I'm
-          <span className="text-[#915eff]">_Sabah</span>
+          <div className="text-left">Hi</div>
+          <div>
+            I'm <span className="text-[#915eff]">_Sabah</span>
+          </div>
         </h1>
-        <div className=" text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-white-100">
+        <div className="text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-4 mr-10 text-right">
           I am:
           <Typewriter
             onInit={(typewriter) => {
@@ -25,20 +29,20 @@ export default function Home() {
               typewriter
                 .typeString("software engineer")
                 .pauseFor(2500)
-                .deleteChars(18)
+                .deleteChars(19)
                 .start();
               typewriter
-                .typeString("n app developer")
+                .typeString("an app developer")
+                .pauseFor(2500)
+                .deleteChars(16)
+                .start();
+              typewriter
+                .typeString("a raccoon lover")
                 .pauseFor(2500)
                 .deleteChars(15)
                 .start();
               typewriter
-                .typeString(" raccoon lover")
-                .pauseFor(2500)
-                .deleteChars(13)
-                .start();
-              typewriter
-                .typeString("brain and computer enthusiast.")
+                .typeString("a brain and computer enthusiast.")
                 .pauseFor(2500)
                 .stop();
             }}
---
+++ components/SabahAvatar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Spline from "@splinetool/react-spline/next";
+
+const SabahAvatar = () => {
+  return (
+    <div className="">
+      <div style={{ height: '100vh' , width: '100vh'}}>
+        <Spline scene="https://prod.spline.design/cXbFeyg3QF12N0ZU/scene.splinecode" />
+      </div>
+    </div>
+  );
+};
+
+export default SabahAvatar;
