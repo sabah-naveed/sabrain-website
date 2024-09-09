9addc349c9b97604b7de2684b351262b5f2bdc82
sabah-naveed <sabahnaveed03@gmail.com>
2024-09-09 14:18:50 -0400

    feat: timeline

--- app/work/page.js
+++ app/work/page.js
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 import React from "react";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 import { motion } from "framer-motion";
 import { textVariant } from "/public/utils/motion.js";
 import { experiences } from "/public/constants/index.js";
- 
 
 const WorkPage = () => {
   return (
@@ -21,15 +21,13 @@ const WorkPage = () => {
       </div>
       <div className="relative z-10 ml-10">
         <motion.div variants={textVariant()}>
-          <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
-            What I have done so far
-          </p>
+          
           <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
             Work Experience.
           </h2>
         </motion.div>
         <div className="mt-20 flex flex-col">
-          <VerticalTimeline>
+          <VerticalTimeline lineColor="white">
             {experiences.map((experience, index) => (
               <ExperienceCard
                 key={`experience-${index}`}
@@ -44,42 +42,45 @@ const WorkPage = () => {
 };
 
 const ExperienceCard = ({ experience }) => (
-  <VerticalTimelineElement
-    contentStyle={{ background: "#1d1836", color: "#fff" }}
-    contentArrowStyle={{ borderRight: "7px solid  #232631" }}
-    date={experience.date}
-    iconStyle={{ background: experience.iconBg }}
-    icon={
-      <div className="flex justify-center items-center w-full h-full">
-        <img
-          src={experience.icon}
-          alt={experience.company_name}
-          className="w-[60%] h-[60%] object-contain"
-        />
-      </div>
-    }
-  >
-    <div>
-      <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+  console.log(experience),
+  (
+    <VerticalTimelineElement
+      visible={true}
+      className="vertical-timeline-element--work"
+      contentStyle={{ background: "#1d1836", color: "#fff" }}
+      contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+      date={experience.date}
+      iconStyle={{ background: experience.iconBg }}
+      icon={
+        <div className="flex justify-center items-center w-full h-full">
+          <img
+            src={experience.icon}
+            alt={experience.company_name}
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+      }
+    >
+      <h3 className="vertical-timeline-element-title">{experience.title}</h3>
       <p
-        className="text-secondary text-[16px] font-semibold"
+        className="vertical-timeline-element-subtitle"
         style={{ margin: 0 }}
       >
         {experience.company_name}
       </p>
-    </div>
 
-    <ul className="mt-5 list-disc ml-5 spaces-y-2">
-      {experience.points.map((point, index) => (
-        <li
-          key={`experience-point-${index}`}
-          className="text-white-100 text-[14px] pl-1 tracking-wider"
-        >
-          {point}
-        </li>
-      ))}
-    </ul>
-  </VerticalTimelineElement>
+      <ul className="mt-5 list-disc ml-5 spaces-y-2">
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className="text-white-100 text-[14px] pl-1 tracking-wider"
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  )
 );
 
 export default WorkPage;
